8e6bc2d0ca79744df76aae1c3c385b48d212a1c4
will <contatowilliamsj@gmail.com>
2024-04-05 20:11:01 -0300

    feat: also works, but cant save to s3

--- processEmail.js
+++ processEmail.js
@@ -1,36 +1,31 @@
 "use strict";
 
-const AWS = require("aws-sdk");
+const AWS = require("@aws-sdk/client-s3");
 const simpleParser = require("mailparser").simpleParser;
 
 const s3 = new AWS.S3({
-  apiVersion: "2006-03-01",
   region: process.env.AWSREGION,
 });
 
 module.exports.handler = async (event) => {
+  console.log("Region:", process.env.AWSREGION);
   console.log("Received event:", JSON.stringify(event, null, 2));
 
   const record = event.Records[0];
-  // Retrieve the email from your bucket
+
   const request = {
     Bucket: record.s3.bucket.name,
     Key: record.s3.object.key,
   };
 
-  try {
-    const data = await s3.getObject(request).promise();
-    const email = await simpleParser(data.Body);
+  const data = await s3.getObject(request);
+  const email = await simpleParser(data.Body);
 
-    console.log("date:", email.date);
-    console.log("subject:", email.subject);
-    console.log("body:", email.text);
-    console.log("from:", email.from.text);
-    console.log("attachments:", email.attachments);
+  console.log("date:", email.date);
+  console.log("subject:", email.subject);
+  console.log("body:", email.text);
+  console.log("from:", email.from.text);
+  console.log("attachments:", email.attachments);
 
-    return { status: "success" };
-  } catch (args) {
-    console.log(args, args.stack);
-    return args;
-  }
+  return { status: "success" };
 };
